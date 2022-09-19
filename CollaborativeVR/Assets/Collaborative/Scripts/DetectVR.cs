04d7c43c6416b4ae8319f414d1a30555f1661925
Geo <petergeorghe.julca@enti.cat>
2022-09-19 14:37:31 +0200

    ExplorativeMap - Walking Around
    - Map where pancake is working 
    - Swimming Pool Gray Boxing

--- CollaborativeVR/Assets/Collaborative/Scripts/DetectVR.cs
+++ CollaborativeVR/Assets/Collaborative/Scripts/DetectVR.cs
@@ -5,38 +5,54 @@ using UnityEngine.XR.Management;
 
 public class DetectVR : MonoBehaviour
 {
+    public bool startInVR;
+    
     public GameObject xrOrigin;
+    public GameObject xrController;
 
     public GameObject desktopCharacter;
     
     // Start is called before the first frame update
     void Start()
     {
-        var xrSettings = XRGeneralSettings.Instance;
-        if (xrSettings == null)
+        if (startInVR == true)
         {
-            Debug.Log("XRGeneralSettings is null");
-            return;
+            var xrSettings = XRGeneralSettings.Instance;
+            if (xrSettings == null)
+            {
+                Debug.Log("XRGeneralSettings is null");
+                return;
+            }
+
+            var xrManager = xrSettings.Manager;
+            if (xrManager == null)
+            {
+                Debug.Log("XRManagerSettings is null");
+                return;
+            }
+
+            var xrLoader = xrManager.activeLoader;
+            if (xrLoader == null)
+            {
+                Debug.Log("XRLoader is null");
+                SwapToDesktopCharacter();
+                return;
+            }
+
+            Debug.Log("XRLoader is NOT null");
+            xrOrigin.SetActive(true);
+            desktopCharacter.SetActive(false);
         }
-        
-        var xrManager = xrSettings.Manager;
-        if (xrManager == null)
+        else
         {
-            Debug.Log("XRManagerSettings is null");
-            return;
+            SwapToDesktopCharacter();
         }
-        
-        var xrLoader = xrManager.activeLoader;
-        if (xrLoader == null)
-        {
-            Debug.Log("XRLoader is null");
-            xrOrigin.SetActive(false);
-            desktopCharacter.SetActive(true);
-            return;
-        }
-        
-        Debug.Log("XRLoader is NOT null");
-        xrOrigin.SetActive(true);
-        desktopCharacter.SetActive(false);
+    }
+
+    void SwapToDesktopCharacter()
+    {
+        xrOrigin.SetActive(false);
+        xrController.SetActive(false);
+        desktopCharacter.SetActive(true);
     }
 }
